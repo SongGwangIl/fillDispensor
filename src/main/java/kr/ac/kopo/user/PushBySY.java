480533900747552ef47c4e1af7c67e277274f093
SiOsy02 <osy0556@gmail.com>
2024-06-17 17:35:34 +0900

    석영 연습파일 두번째 push

---
+++ src/main/java/kr/ac/kopo/user/PushBySY.java
@@ -0,0 +1,20 @@
+package kr.ac.kopo.user;
+
+public class PushBySY {
+	private String hell;
+
+	
+	public String getHell() {
+		return hell;
+	}
+
+	public void setHell(String hell) {
+		this.hell = hell;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("진짜로 어렵습니다.");
+	}
+
+}
