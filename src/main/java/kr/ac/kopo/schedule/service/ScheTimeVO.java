698ae272885ea5d69e0a0c3f1d0a383e8b2e0b21
you-so-good <ekdud9424@naver.com>
2024-07-04 17:01:55 +0900

    다영 - scheTime 빠진 필드 하나 추가

--- src/main/java/kr/ac/kopo/schedule/service/ScheTimeVO.java
+++ src/main/java/kr/ac/kopo/schedule/service/ScheTimeVO.java
@@ -10,6 +10,7 @@ public class ScheTimeVO {
 
 	private String timeId;
 	private String scheId;
+	private String timeName;
 	private LocalTime timeArlam;
 	private LocalTime timeLimit;
 	private int reArlamCount;
