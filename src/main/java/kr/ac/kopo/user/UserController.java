d37dd8647e5b04bdea23b808494449a002f22d6c
Student <Student@Student-PC>
2024-08-22 20:37:19 +0900

    송광일 8/22 주석

--- src/main/java/kr/ac/kopo/user/UserController.java
+++ src/main/java/kr/ac/kopo/user/UserController.java
@@ -20,41 +20,43 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
-	
+	// 초기화면요청
 	@RequestMapping("/cover")
 	public String cover() {
 		return "user/cover";
 	}
-	
+	// 로그인 화면요청
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
 	public String loginForm() {
 		
 		return "user/login";
 	}
-
+	// 로그인 요청
 	@RequestMapping(path = "/login", method = RequestMethod.POST)
 	public String login(@ModelAttribute("userId") UserVO vo, HttpSession session) {
 			
 		UserVO uvo = userService.login(vo.getUserId());
 		String name = null;
-		
+		//name에 사용자이름또는 보호자이름 저장
 		if(uvo != null && uvo.getUserSelect().equals("user"))
 			name = userService.getUserName(vo.getUserId());
 		else if(uvo != null && uvo.getUserSelect().equals("protector"))
 			name = userService.getProtectorName(vo.getUserId());
 		
-		
+		//로그인
 		if ( uvo != null && vo.getUserId().equals(uvo.getUserId()) && 
 				vo.getUserPwd().equals(uvo.getUserPwd()) ) 
 		{
 			//로그인 성공
 			session.setAttribute("loginUser", uvo);
+			//정보미입력시 입력창 이동
 			if(name == null) {
 				if(uvo.getUserSelect().equals("user"))				
 					return "user/addUserInfo";				
 				else 
 					return "user/addProtectorInfo";				
-			}				
+			}
+			//정보입력완료시 메인창 이동
 			else {
 				session.setAttribute("name", name);
 				
@@ -64,26 +66,26 @@ public class UserController {
 		// 로그인 실패	
 		return "redirect:/login";
 	}
-	
+	//로그아웃 요청
 	@RequestMapping(path = "/logout", method = RequestMethod.GET)
 	public String logout(HttpSession session) {
 		session.invalidate();
 		
 		return "redirect:/cover";
 	}
-	
+	//약관동의 화면요청
 	@RequestMapping(path = "/terms", method = RequestMethod.GET)
 	public String terms() {
 		
 		return "user/terms";
 	}
-	
+	//회원가입 화면요청
 	@RequestMapping(path = "/singup", method = RequestMethod.GET)
 	public String addForm() {
 		
 		return "user/add";
 	}
-	
+	//회원가입 
 	@RequestMapping(path = "/singup", method = RequestMethod.POST)
 	public String add(UserVO vo, HttpServletRequest request) {
 		
@@ -94,7 +96,7 @@ public class UserController {
 		
 		return "user/msg";			
 	}
-	
+	//아이디 사용유무확인 ajax 요청,응답
 	@ResponseBody
 	@RequestMapping(path = "/checkId", method = RequestMethod.POST)
 	public String checkId(String userId) {
@@ -102,13 +104,13 @@ public class UserController {
 		
 		return result;
 	}
-	
+	//보호자 정보입력 화면요청
 	@RequestMapping(path = "/addProtectorInfo", method = RequestMethod.GET)
 	public String ProtectorInfo() {
 		
 		return "user/addProtectorInfo";
 	}
-	
+	//보호자 정보입력
 	@RequestMapping(path = "/addProtectorInfo", method = RequestMethod.POST)
 	public String addProtectorInfo(UserInfoVO vo, HttpServletRequest request) {
 		
@@ -118,15 +120,14 @@ public class UserController {
 		request.setAttribute("url", "/kopo/main");
 		
 		return "user/msg";
-	}
-	
-	
+	}	
+	//사용자 정보입력 화면요청
 	@RequestMapping(path = "/addUserInfo", method = RequestMethod.GET)
 	public String UserInfo() {
 		
 		return "user/addUserInfo";
 	}
-
+	//사용자 정보입력
 	@RequestMapping(path = "/addUserInfo", method = RequestMethod.POST)
 	public String addUserInfo(UserInfoVO vo, HttpServletRequest request) {
 		
@@ -137,25 +138,27 @@ public class UserController {
 		
 		return "user/msg";
 	}
+	//나의정보변경 화면 요청
 	@GetMapping("/myPage")
 	public String myPage(@SessionAttribute("loginUser") UserVO vo, Model model) {
 		String userId = vo.getUserId();
 		String userValid = userService.getUserValid(userId);
 		String userSelect = userService.getUserSelect(userId);
-		
+		//정보 미 등록시 등록화면 이동
 		if(userValid.equals("F")) {
 			if(userSelect.equals("user"))
 				return "user/addUserInfo";
 			else
 				return "user/addProtectorInfo";
 		}
-		
+		//정보 등록되어있는 경우 변경화면 이동
 		UserInfoVO uivo = userService.getUserInfo(vo);
 		
 		model.addAttribute("myInfo", uivo);
 		
 		return "user/myPage";
 	}
+	//나의정보변경
 	@PostMapping("/myPage")
 	public String myPage(UserInfoVO uivo, HttpSession session) {
 		
