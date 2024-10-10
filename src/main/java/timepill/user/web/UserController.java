7adce41419b34a30c753889669c3333fa3b6deba
Student <Student@Student-PC>
2024-10-10 14:48:17 +0900

    todoCnt 크기 위치 수정

--- src/main/java/timepill/user/web/UserController.java
+++ src/main/java/timepill/user/web/UserController.java
@@ -47,7 +47,7 @@ public class UserController {
 	/** 로그아웃(시큐리티 처리) */
 	@PostMapping("/user/logout")
 	public void logout() throws Exception {
-		kakaoService.logout();
+		kakaoService.logout(); //유저 로그아웃
 	}
 	
 	/** 약관동의 화면요청 */
@@ -70,7 +70,7 @@ public class UserController {
 		if(result.hasErrors())
 			return "user/Add";
 		
-		userService.add(vo);
+		userService.add(vo); //유저정보 DB추가
 		
 		request.getSession().setAttribute("message", "회원가입 되었습니다.");
 		
@@ -82,52 +82,56 @@ public class UserController {
 	@PostMapping("/user/check-id")
 	public String checkId(String userId) {
 		
-		String result = userService.checkId(userId);
+		String result = userService.checkId(userId); // DB에 존재하는 userId인지 확인
 		
 		return result;
 	}
 	
+	/** 아이디 찾기 페이지 요청 */
 	@GetMapping("/user/find-id")
 	public String findId() {
 		
 		return "user/findId";
 	}
-	
+	/** 아이디 찾기 */
 	@ResponseBody
 	@PostMapping("/user/find-id")
 	public String findId(String email) {
 		
-		String result = userService.findId(email);
+		String result = userService.findId(email); // email정보가 일치하는 id확인
 		
 		return result;
 	}
 	
+	/** 이메일 인증 페이지 요청 */
 	@GetMapping("/user/auth-email")
 	public String authEmail() {
 		
 		return "user/AuthEmail";
 	}
 	
+	/** 이메일 인증번호 발송 */
 	@ResponseBody
 	@PostMapping("/user/auth-email")
 	public String authEmail(AuthVO vo, HttpServletResponse response, Model model) throws Exception {
 		
-		String result = authService.authEmail(vo);
+		String result = authService.authEmail(vo); // 이메일 셋팅 및 전송
 		
 		return result;
 	}
 	
+	/** 이메일 인증 */
 	@PostMapping("/user/auth-atmp")
 	public String authAtmp(AuthVO vo, HttpSession session, Model model) {
 		
-		String result = authService.authAtmp(vo);
+		String result = authService.authAtmp(vo); // 이메일 인증
 		
-		if(result.equals("Y")) {
+		if(result.equals("Y")) { // 인증 성공
 			session.setAttribute("authOK", vo);
 			
 			return "user/ResetPassword";
 		}			
-		else {
+		else { // 인증 실패
 			model.addAttribute("message", "인증에 실패했습니다.");
 			
 			return "user/AuthEmail";
@@ -136,10 +140,11 @@ public class UserController {
 		
 	}
 	
+	/** 이메일 인증 확인 */
 	@GetMapping("/user/reset-password")
 	public String resetPassword(HttpSession session) {
 		
-		if(session.getAttribute("authOK") == null) {
+		if(session.getAttribute("authOK") == null) { // 인증여부 확인
 			session.setAttribute("message", "인증정보가 없습니다.");
 			
 			return "/user/Auth-email";
@@ -148,9 +153,11 @@ public class UserController {
 		return "user/ResetPassword";
 	}
 	
+	/** 패스워드 변경 */
 	@PostMapping("/user/reset-password")
 	public String resetPassword(UserVO vo, HttpSession session) {
 		
+		// 인증된 정보와 패스워드로 DB내용 변경
 		AuthVO avo = (AuthVO)session.getAttribute("authOK");
 		session.removeAttribute("authOK");
 		vo.setUserId(avo.getUserId());
@@ -162,52 +169,57 @@ public class UserController {
 		return "redirect:/user/login";		
 	}
 	
-	
+	/** 마이페이지 요청 */
 	@GetMapping("/mypage")
 	public String myPage() {
 		
 		return "user/MyPage";
 	}
 	
+	/** 내 정보 변경 페이지 요청 */
 	@PostMapping("/mypage")
 	public String myPage(String password, UserVO vo, HttpSession session) {
 		String userId = SecurityContextHolder.getContext().getAuthentication().getName();
 		vo.setUserId(userId);
 		
-		UserVO uvo = userService.getMyInfo(vo);
-		boolean isMatch = encoder.matches(password, uvo.getPassword());
+		UserVO uvo = userService.getMyInfo(vo); // 내정보 가져오기
+		boolean isMatch = encoder.matches(password, uvo.getPassword()); // 비밀번호 일치여부 확인
 		
-		if(isMatch)
+		if(isMatch) // 비밀번호 일치
 			return "redirect:/changeMyInfo";
-		else
+		else // 비밀번호 불일치
 			session.setAttribute("message", "비밀번호가 일치하지 않습니다.");
 		return "user/Mypage";
 	}
 	
+	/** 비밀번호 변경페이지 요청 */
 	@GetMapping("/mypage/change-Password")
 	public String changePw() {
+		
 		return "user/ResetPassword";
 	}
 	
+	/** 내 정보 변경 페이지 요청 */
 	@GetMapping("/mypage/change-myinfo")
 	public String changeMyInfo(UserVO vo, Model model) {
 		String userId = SecurityContextHolder.getContext().getAuthentication().getName();
-		vo.setUserId(userId);
-		System.out.println(vo.getPassword());
-		UserVO uvo = userService.getMyInfo(vo);
+		vo.setUserId(userId); // 시큐리티에서 유저 아이디 가져와 셋팅
+		UserVO uvo = userService.getMyInfo(vo); // 유저 정보 가져오기
 		
-		model.addAttribute("userVO", uvo);
+		model.addAttribute("userVO", uvo); // 기존유저정보 셋팅
 		
 		return "user/ChangeMyInfo";
 	}
 	
+	/** 내 정보 변경 */
 	@PostMapping("/mypage/change-myinfo")
 	public String chageMyInfo(UserVO vo, BindingResult result, Model model) {
 		
+		// 검증결과 error가 있으면
 		if(result.hasErrors())
 			return "changeMyInfo";
 		
-		userService.changeMyInfo(vo);
+		userService.changeMyInfo(vo); // 유저정보변경
 		model.addAttribute("message", "변경되었습니다.");
 		
 		return "redirect:/";
